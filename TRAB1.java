89b4150aba5245205e0f6b54d7aa75a35c06554f
jvSemblano <36772001+jvSemblano@users.noreply.github.com>
2018-02-24 22:21:53 +0000

    Add files via upload

--- TRAB1.java
+++ TRAB1.java
@@ -10,29 +10,46 @@ class Node{
     private int pai;
     private int custo;
     private char jogada;
+    private int positionY;
+    private int positionX;
     
-    Node(int[][] matrix2, int prof, int pai, int cost, char jogada){
-    	matrix = matrix2;
+    Node(int[][] matrix, int prof, int pai, int cost, char jogada, int positionX, int positionY){
+    	this.matrix = matrix;
     	profundidade = prof;
     	this.pai = pai;
     	custo = cost;
     	this.jogada = jogada;
+    	this.positionY = positionY;
+    	this.positionX = positionX;
     }
 
-    public void printJogada(){
-    	System.out.print(jogada + " ");
+    public char getJogada(){
+    	return jogada;
     }
-    
+
+    public int[][] getTable(){
+    	return matrix;
+    }
+
+    public int getX(){
+    	return positionX;
+    }
+
+    public int getY(){
+    	return positionY;
+    }
 }
 
 public class TRAB1{
 
-	static int positionX = 0;
-	static int positionY = 0;
+
 	static int[][] complete = {{1,2,3,4}, 
 							   {5,6,7,8}, 
 							   {9, 10, 11, 12}, 
 							   {13, 14, 15, 0}};
+	static LinkedList<Node> queue = new LinkedList<Node>();
+
+
 
 	static boolean checkIfPossible(int[] array){
 		int inversions = 0;
@@ -79,35 +96,64 @@ public class TRAB1{
 
     }*/
 
-    /*static void generalSearchAlgorithm( String qualquer, int[][] jogoinicial, int[][] jogofinal){
-    	if()
-    }*/
+    static String generalSearchAlgorithm(String funcao, Node jogoinicial, int[][] jogofinal){
+    	String moves = "";
+    	queue.add(jogoinicial);
+    	while(!queue.isEmpty()){
+    		Node temporary = queue.remove();
+    		moves += temporary.getJogada() + " ";
+    		if(Arrays.deepEquals(temporary.getTable(),jogofinal)){
+    			return moves;
+    		}	
+    		//construir os filhos e inseri-los todos na queue
+    		return "solution not found";
+    	}
+    }
 
-    static void moveUp(int[][] gamematrix){
-		if(positionY != 0){
-			gamematrix[positionY][positionX] = gamematrix[positionY - 1][positionX];
-			gamematrix[positionY - 1][positionX] = 0;
+    static void 
+
+    static void moveUp(Node game){
+		if(game.getY != 0){
+			int positionY = game.getY();
+			int positionX = game.getX();
+			int[][] temp = game.getTable();
+			temp[positionY][positionX] = temp[positionY - 1][positionX];
+			temp[positionY - 1][positionX] = 0;
+			//nodes.add(new Node(temp, int prof, int pai, int cost, 'U'));
 		}
 	}
 
-	static void moveDown(int[][] gamematrix){
-		if(positionY != 3) {
-			gamematrix[positionY][positionX] = gamematrix[positionY + 1][positionX];
-			gamematrix[positionY + 1][positionX] = 0;
+
+	static void moveDown(int[][] temp){
+		if(game.getY != 3) {
+			int[][] temp = game.getTable();
+			int positionY = game.getY();
+			int positionX = game.getX();
+			temp[positionY][positionX] = temp[positionY + 1][positionX];
+			temp[positionY + 1][positionX] = 0;
+			//nodes.add(new Node(temp, int prof, int pai, int cost, 'D'));
 		}	
 	}
 
-	static void moveLeft(int[][] gamematrix){
-		if(positionX != 0) {
-			gamematrix[positionY][positionX] = gamematrix[positionY][positionX - 1];
-			gamematrix[positionY][positionX - 1] = 0;
+	static void moveLeft(int[][] temp){
+		if(game.getX != 0) {
+			int[][] temp = game.getTable();
+			int positionY = game.getY();
+			int positionX = game.getX();
+			temp[positionY][positionX] = temp[positionY][positionX - 1];
+			temp[positionY][positionX - 1] = 0;
+			//nodes.add(new Node(temp, int prof, int pai, int cost, 'L'));
 		}	
 	}
 
-	static void moveRight(int[][] gamematrix){
-		if(positionX != 3) {
-			gamematrix[positionY][positionX] = gamematrix[positionY][positionX + 1];
-			gamematrix[positionY][positionX + 1] = 0;
+	static void moveRight(int[][] temp){
+		if(game.getX != 3) {
+			int[][] temp = game.getTable();
+			int positionY = game.getY();
+			int positionX = game.getX();
+			temp[positionY][positionX] = temp[positionY][positionX + 1];
+			temp[positionY][positionX + 1] = 0;
+			//nodes.add(new Node(temp, int prof, int pai, int cost, 'R'));
 		}
 	}
 
@@ -118,15 +164,16 @@ public class TRAB1{
 
     	int[] arrayToCheck = new int[16];
     	int[][] configinicial = new int[4][4];
-    	int[][] matrix = new int [4][4];
-
-
+    	
+    	
+    
     	for(int i = 0; i < arrayToCheck.length; i++){
     		arrayToCheck[i] = input.nextInt();
     	}
 
     	if(checkIfPossible(arrayToCheck)){
-
+    		int positionY;
+    		int positionX;
     		int i = 0;
     		for(int j = 0; j < 4; j++){
     			for(int z = 0; z < 4; z++){
@@ -138,6 +185,8 @@ public class TRAB1{
     				i++;
     			}
     		}
+
+    		nodes.add(new Node(configinicial, 0, NULL, 1, NULL, positionX, positionY))
    			 		
        	}	
 
@@ -145,7 +194,7 @@ public class TRAB1{
     		System.out.println("O puzzle e impossivel de resolver!..."); 
     	}
     	
-
+    	return;
     
     }
 }
