f9a24038726750369538738588c8b06468a8a04d
solsampaio18 <36776822+solsampaio18@users.noreply.github.com>
2018-02-27 20:27:25 +0000

    Add files via upload

--- TRAB1.java
+++ TRAB1.java
@@ -1,27 +1,25 @@
 
-import java.util.*;
 import java.lang.*;
-
+import java.util.*;
 
 
 class Node{
     private int[][] matrix;
-    private int profundidade;
-    private int pai;
-    private int custo;
-    private char jogada;
+    int profundidade = 0;
+    Node pai = null;
+    int custo = 1;
+    char jogada = ' ';
     private int positionY;
     private int positionX;
     
-    Node(int[][] matrix, int prof, int pai, int cost, char jogada, int positionX, int positionY){
+    Node(int[][] matrix, int positionX, int positionY){
     	this.matrix = matrix;
-    	profundidade = prof;
-    	this.pai = pai;
-    	custo = cost;
-    	this.jogada = jogada;
     	this.positionY = positionY;
     	this.positionX = positionX;
     }
+
+    
+    
 
     public char getJogada(){
     	return jogada;
@@ -41,8 +39,10 @@ class Node{
 }
 
 public class TRAB1{
+        static int positionY;
+    	static int positionX;
+        static String moves = "";
 
-	static LinkedList<Node> queue = new LinkedList<Node>();
 
 	static boolean checkIfPossible(int[] array){
 		int inversions = 0;
@@ -76,88 +76,196 @@ public class TRAB1{
 			return false;
 		}
 	}
+ 
+    static boolean compareMatrix(int[][] array1, int[][] array2){
+	for(int i = 0; i < array1[0].length; i++){
+	    for(int j = 0; j < 4; j++){
+		if(array1[i][j] != array2[i][j]){
+		    return false;
+		}
+	    }
+	}
+
+	return true;
+    }
 
 
     /*static void IDFS(){
 	
     }
-
     static void GULOSA(){
+    }
+    static void A(){
+    }*/
+
+    static void BFS(Node jogoinicial, Node jogofinal){
+    	LinkedList<Node> visitados = new LinkedList<Node>();
+    	LinkedList<Node> filhotes = new LinkedList<Node>();
+    	LinkedList<Node> queue = new LinkedList<Node>();
+
+    	queue.add(jogoinicial);
+    	
+    	while(!queue.isEmpty()){
+    		Node temporary = queue.remove();
+    		System.out.println(Arrays.deepToString(temporary.getTable()) + " " + temporary.profundidade);
+    		//moves += temporary.getJogada() + " ";
+    		if(compareMatrix(temporary.getTable(), jogofinal.getTable())){
+    		
+
+    			System.out.println("solucao");
+    			//moves += "\n" + "Profundidade = " + temporary.profundidade;
+    			System.out.println(moves);
+    			return;
+    		}	
+    		filhotes = makeDescendants(temporary);
+    		int conclude = 0;
+    		for(int i = 0; i < filhotes.size(); i++){
+    			for(int j = 0; j < visitados.size(); j++){
+    				if(compareMatrix(filhotes.get(i).getTable(), visitados.get(j).getTable())){
+    					conclude = 1;
+    					break;
+    				}
+    			}
+    			if(conclude == 0){
+    				visitados.add(filhotes.get(i));
+    				queue.add(filhotes.get(i));
+    			}
+    			conclude = 0;
+
+    		}	
+    	}
+
+    	System.out.println("solution not found");
+    	return;
 
     }
 
-    static void A(){
-
-    }*/
-
-   static String generalSearchAlgorithm(String funcao, Node jogoinicial, int[][] jogofinal){
-    	String moves = "";
+    static void DFS (Node jogoinicial, Node jogofinal){
+	LinkedList<Node> visitados = new LinkedList<Node>();
+	LinkedList<Node> filhotes = new LinkedList<Node>();
+	LinkedList<Node> queue = new LinkedList<Node>();
+	
     	queue.add(jogoinicial);
     	while(!queue.isEmpty()){
     		Node temporary = queue.remove();
     		moves += temporary.getJogada() + " ";
-    		if(Arrays.deepEquals(temporary.getTable(),jogofinal)){
-    			return moves;
+    		if(compareMatrix(temporary.getTable(), jogofinal.getTable())){
+		     moves += "\n" + "Profundidade = " + temporary.profundidade;
+		    System.out.println(moves);
+		    return;
     		}	
-    		//construir os filhos e inseri-los todos na queue
-    		return "solution not found";
+    		filhotes = makeDescendants(temporary);
+		int conclude = 0;
+		for(int i = 0; i < filhotes.size(); i++){
+		    for(int j = 0; j < visitados.size(); j++){
+			if(compareMatrix(filhotes.get(i).getTable(), visitados.get(j).getTable())){
+			    conclude = 1;
+			    break;
+   			}
+		    }
+		    if(conclude == 0){
+			queue.addFirst(filhotes.get(i));
+		    }
+		    conclude = 0;
+		    
+		}
     	}
+
+	System.out.println("solution not found");
+	return;
     }
 
-    static void moveUp(Node game){
-		if(game.getY != 0){
-			int positionY = game.getY();
-			int positionX = game.getX();
-			int[][] temp = game.getTable();
-			temp[positionY][positionX] = temp[positionY - 1][positionX];
-			temp[positionY - 1][positionX] = 0;
-			//nodes.add(new Node(temp, int prof, int pai, int cost, 'U'));
+
+    static Node moveUp(Node game){
+		
+	int positionY = game.getY();
+	int positionX = game.getX();
+	int[][] temp = new int[4][4];
+		for(int i = 0; i < 4; i++){
+		for(int j=0 ; j<4; j++) {
+			temp[i][j]=game.getTable()[i][j];
 		}
 	}
-
-
-	static void moveDown(Node game){
-		if(game.getY != 3) {
-			int[][] temp = game.getTable();
-			int positionY = game.getY();
-			int positionX = game.getX();
-			temp[positionY][positionX] = temp[positionY + 1][positionX];
-			temp[positionY + 1][positionX] = 0;
-			//nodes.add(new Node(temp, int prof, int pai, int cost, 'D'));
-		}	
+	temp[positionY][positionX] = temp[positionY - 1][positionX];
+	temp[positionY - 1][positionX] = 0;
+	Node toReturn = new Node(temp,positionX, positionY - 1);
+	toReturn.pai = game;
+	toReturn.jogada = 'U';
+	toReturn.profundidade = game.profundidade + 1;
+	return toReturn;
+	
+	      
 	}
 
-	static void moveLeft(Node game){
-		if(game.getX != 0) {
-			int[][] temp = game.getTable();
-			int positionY = game.getY();
-			int positionX = game.getX();
-			temp[positionY][positionX] = temp[positionY][positionX - 1];
-			temp[positionY][positionX - 1] = 0;
-			//nodes.add(new Node(temp, int prof, int pai, int cost, 'L'));
-		}	
-	}
 
-	static void moveRight(Node game){
-		if(game.getX != 3) {
-			int[][] temp = game.getTable();
-			int positionY = game.getY();
-			int positionX = game.getX();
-			temp[positionY][positionX] = temp[positionY][positionX + 1];
-			temp[positionY][positionX + 1] = 0;
-			//nodes.add(new Node(temp, int prof, int pai, int cost, 'R'));
+	static Node moveDown(Node game){
+		
+	   
+	    int positionY = game.getY();
+	    int positionX = game.getX();
+	    int[][] temp = new int[4][4];
+		for(int i = 0; i < 4; i++){
+		 for(int j=0 ; j<4; j++) {
+			temp[i][j]=game.getTable()[i][j];
 		}
+	}
+	    temp[positionY][positionX] = temp[positionY + 1][positionX];
+	    temp[positionY + 1][positionX] = 0;
+	   	Node toReturn = new Node(temp,positionX, positionY + 1);
+		toReturn.pai = game;
+		toReturn.jogada = 'D';
+		toReturn.profundidade = game.profundidade + 1;
+		return toReturn;
+	       	
+	}
+
+	static Node moveLeft(Node game){
+	
+	   
+	    int positionY = game.getY();
+	    int positionX = game.getX();
+	    int[][] temp = new int[4][4];
+		for(int i = 0; i < 4; i++){
+		 for(int j=0 ; j<4; j++) {
+			temp[i][j]=game.getTable()[i][j];
+		}
+	}
+	    temp[positionY][positionX] = temp[positionY][positionX - 1];
+	    temp[positionY][positionX - 1] = 0;
+	    	Node toReturn = new Node(temp,positionX - 1, positionY);
+		toReturn.pai = game;
+		toReturn.jogada = 'L';
+		toReturn.profundidade = game.profundidade + 1;
+		return toReturn;
+		
+	}
+
+	static Node moveRight(Node game){
+	    int positionY = game.getY();
+	    int positionX = game.getX();
+	    int[][] temp = new int[4][4];
+		for(int i = 0; i < 4; i++){
+		 for(int j=0 ; j<4; j++) {
+			temp[i][j]=game.getTable()[i][j];
+		}
+	}
+	    temp[positionY][positionX] = temp[positionY][positionX + 1];
+	    temp[positionY][positionX + 1] = 0;
+	    	Node toReturn = new Node(temp,positionX + 1, positionY);
+		toReturn.pai = game;
+		toReturn.jogada = 'R';
+		toReturn.profundidade = game.profundidade + 1;
+		return toReturn;
 	}
 
 	
-    	static void arrayToMatrix(int[] array, int[] configinicial){
-		int positionY;
-    		int positionX;
+    	static void arrayToMatrix(int[] array, int[][] config){
+	       
     		int i = 0;
     		for(int j = 0; j < 4; j++){
     			for(int z = 0; z < 4; z++){
-    				configinicial[j][z] = arrayToCheck[i];
-    				if(configinicial[j][z] == 0){
+    				config[j][z] = array[i];
+    				if(config[j][z] == 0){
     					positionY = j;
     					positionX = z;
     				}
@@ -165,57 +273,98 @@ public class TRAB1{
     			}
     		}
 
-    		nodes.add(new Node(configinicial, 0, NULL, 1, NULL, positionX, positionY));
+    		
 	}
     
-	static void menu(){
-		System.out.println("Qual algoritmo quer usar?");
-		System.out.println("IDFS   -> 1");
-		System.out.println("Greddy -> 2");
-		System.out.println("A_STAR -> 3");
-		Scanner input = new Scanner(System.in);
-		int opcao = input.nextInt();
-		switch(opcao){
-			case 1: generalSearchAlgorithm("IDFS", nodes.get(0); jogofinal);
-				break;
-				
-			case 2: generalSearchAlgorithm("Greedy", nodes.get(0); jogofinal);
-				break;
-				
-			case 3: generalSearchAlgorithm("A_STAR", nodes.get(0); jogofinal);
-				break;
-				
-			default: System.out.println("Escolha uma das opcoes do menu");
-				 break;		    
-				
-		}
+    static LinkedList<Node> makeDescendants(Node pai){
+	LinkedList<Node> children = new LinkedList<Node>();
+	if(pai.getY() != 0){
+	    children.add(moveUp(pai));
 	}
+
+	if(pai.getY() != 3){
+	    children.add(moveDown(pai));
+	}
+
+	if(pai.getX() != 0){
+	    children.add(moveLeft(pai));
+	}
+
+	if(pai.getX() != 3){
+	    children.add(moveRight(pai));
+	}
+
+	return children;
+    }
+    
+    static void menu(Node jogoinicial, Node jogofinal){
+	System.out.println("Qual algoritmo quer usar?");
+	System.out.println("IDFS   -> 1");
+	System.out.println("Greedy -> 2");
+	System.out.println("A_STAR -> 3");
+	System.out.println("BFS    -> 4");
+	System.out.println("DFS    -> 5");
+	Scanner input = new Scanner(System.in);
+	int opcao = input.nextInt();
+	switch(opcao){
+	    /*case 1: IDFS(jogoinicial, jogofinal);
+	        break;
+				
+	case 2: SERANATADEAMOR(jogoinicial, jogofinal);
+	        break;
+				
+	case 3: A_STAR(jogoinicial, jogofinal);
+	break;*/
+
+	case 4: BFS(jogoinicial , jogofinal);
+	        break;
+	    
+	case 5: DFS(jogoinicial , jogofinal);
+	        break;
+				
+	default: System.out.println("Escolha uma das opcoes do menu!");
+	    break;		    		
+	}
+    }
 
     
     public static void main(String[] args){
        	System.out.println("Bem-vindo ao Jogo dos 15!");
-	System.out.println("Introduza a configuração inicial do tabuleiro:");
+	System.out.println("Introduza a configuracao inicial do tabuleiro:");
     	Scanner input = new Scanner(System.in);
-    	int[] arrayToCheck = new int[16];
+    	int[] arrayInicial = new int[16];
+	int[] arrayFinal = new int[16];
     	int[][] configinicial = new int[4][4];
+	int[][] configfinal = new int[4][4];
     	
-    	for(int i = 0; i < arrayToCheck.length; i++){
-    		arrayToCheck[i] = input.nextInt();
+    	for(int i = 0; i < arrayInicial.length; i++){
+    		arrayInicial[i] = input.nextInt();
     	}
 
-    	while(!checkIfPossible(arrayToCheck)){
-    		System.out.println("O puzzle e impossivel de resolver!..."); 
-		System.out.println("Insira novamente uma configuracao inicial:");
-		for(int i = 0; i < arrayToCheck.length; i++){
-    			arrayToCheck[i] = input.nextInt();
-    		}
-	}
+	boolean condI = checkIfPossible(arrayInicial);
 		
-	arrayToMatrix(arrayToCheck, configinicial);
+	
 	    
 	System.out.println("Insira agora a configuração final:");
-	//para que serve o algoritmo da dutra se dermos config final?? como testamos se de determinado ponto chegamos aqui?
-	menu();
+	for(int i = 0; i < arrayFinal.length; i++){
+    		arrayFinal[i] = input.nextInt();
+    	}
+	
+	boolean condF = checkIfPossible(arrayFinal);
+	
+	if(condI == condF){
+	    arrayToMatrix(arrayInicial, configinicial);
+	    
+	    Node inicial = new Node(configinicial, positionX, positionY); 
+	    arrayToMatrix(arrayFinal, configfinal);
+	    Node fim = new Node(configfinal, positionX, positionY); 
+	    
+	    menu(inicial, fim);
+	}
+
+	else{
+	    System.out.println("Impossible");
+	}
     	
     	return;
     
